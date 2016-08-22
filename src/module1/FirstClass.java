1452babd8ef79a93ab8f2c98f44a16a12c5488ab
SergeDovbnya <serge.dovbnya@gmail.com>
2016-08-22 12:48:07 +0300

    Homework first module

---
+++ src/module1/FirstClass.java
@@ -0,0 +1,8 @@
+package module1;
+
+
+public class FirstClass {
+    public static void printMessage() {
+        System.out.println("My first programs on Java... ");
+    }
+}
---
+++ src/module1/ThirdClass.java
@@ -0,0 +1,8 @@
+package module1;
+
+
+public class ThirdClass {
+    public static void printMessage() {
+        System.out.println("My first programs on Java... ");
+    }
+}
