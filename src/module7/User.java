4e78c1dff85db6479a699fe64e90372bbeceae92
Serge <serge.dovbnya@gmail.com>
2016-10-07 13:59:30 +0300

    Module 7

---
+++ src/module7/User.java
@@ -0,0 +1,46 @@
+package module7;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
+
+    public User(long id, String firstName, String lastName, String city, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.balance = balance;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (balance != user.balance) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        return city != null ? city.equals(user.city) : user.city == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + balance;
+        return result;
+    }
+}
