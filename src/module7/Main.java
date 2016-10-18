cd200ade81d8687362685c955fc3ffe882dbefd4
Serge <serge.dovbnya@gmail.com>
2016-10-18 14:11:42 +0300

    more modified solutions in module7

--- src/module7/Main.java
+++ src/module7/Main.java
@@ -5,30 +5,31 @@ import module4.*;
 import module4.Currency;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User(1, "Kay", "Sims", "Sydney", 1400);
-        User user1 = new User(2, "Alfred", "Hitchcock", "Los Angeles", 5000);
-        User user2 = new User(3, "Stasey", "Willberg", "Melbourne", 8850);
-        User user3 = new User(4, "Whitney", "Houston", "New Jersey", 4000);
-        User user4 = new User(5, "Barbara", "Streisand", "Malibu", 6050);
-        User user5 = new User(6, "Casey", "Neistat", "New York", 9999);
-        User user6 = new User(7, "Steven", "Byrd", "Essex", 4621);
-        User user7 = new User(8, "Jeffrey", "Garza", "Capetown", 3215);
-        User user8 = new User(9, "Ivan", "Petrov", "Moscow", 7850);
-        User user9 = new User(10, "Amanda", "Gregory", "New York", 2033);
+        User user1 = new User(1, "Kay", "Sims", "Sydney", 1400);
+        User user2 = new User(2, "Alfred", "Hitchcock", "Los Angeles", 5000);
+        User user3 = new User(3, "Stasey", "Willberg", "Melbourne", 8850);
+        User user4 = new User(4, "Whitney", "Houston", "New Jersey", 4000);
+        User user5 = new User(5, "Barbara", "Streisand", "Malibu", 6050);
+        User user6 = new User(6, "Casey", "Neistat", "New York", 9999);
+        User user7 = new User(7, "Steven", "Byrd", "Essex", 4621);
+        User user8 = new User(8, "Jeffrey", "Garza", "Capetown", 3215);
+        User user9 = new User(9, "Ivan", "Petrov", "Moscow", 7850);
+        User user10 = new User(10, "Amanda", "Gregory", "New York", 2033);
         List<Order> list = new ArrayList<>();
-        list.add(new Order(1, 600, Currency.EUR, "IPhone", "Apple", user));
-        list.add(new Order(2, 15, Currency.USD, "Wine", "Spirits & Liquor", user1));
-        list.add(new Order(2, 15, Currency.USD, "Wine", "Spirits & Liquor", user1));
-        list.add(new Order(4, 99, Currency.USD, "Trampoline", "Amazon", user3));
-        list.add(new Order(4, 99, Currency.USD, "Trampoline", "Amazon", user3));
-        list.add(new Order(6, 1500, Currency.USD, "DJI Phantom 4", "DJI", user5));
-        list.add(new Order(7, 15, Currency.USD, "Kitchen Scale", "Ebay", user6));
-        list.add(new Order(8, 700, Currency.EUR, "IPhone", "Apple", user7));
-        list.add(new Order(9, 3999, Currency.USD, "Pinball machine", "Ebay", user8));
-        list.add(new Order(10, 2000, Currency.EUR, "A decent espresso maker", "Amazon", user9));
+        list.add(new Order(1, 600, Currency.EUR, "IPhone", "Apple", user1));
+        list.add(new Order(2, 15, Currency.USD, "Wine", "Spirits & Liquor", user2));
+        list.add(new Order(2, 15, Currency.USD, "Wine", "Spirits & Liquor", user2));
+        list.add(new Order(4, 99, Currency.USD, "Trampoline", "Amazon", user4));
+        list.add(new Order(4, 99, Currency.USD, "Trampoline", "Amazon", user4));
+        list.add(new Order(6, 1500, Currency.USD, "DJI Phantom 4", "DJI", user6));
+        list.add(new Order(7, 15, Currency.USD, "Kitchen Scale", "Ebay", user7));
+        list.add(new Order(8, 700, Currency.EUR, "IPhone", "Apple", user8));
+        list.add(new Order(9, 3999, Currency.USD, "Pinball machine", "Ebay", user9));
+        list.add(new Order(10, 2000, Currency.EUR, "A decent espresso maker", "Amazon", user10));
         Set<Order> set = new TreeSet<>(list);
 
         list.sort(new Comparator<Order>() {
@@ -79,25 +80,27 @@ public class Main {
 //        separate list by Currency
         List<Order> usdList = new ArrayList<>();
         List<Order> eurList = new ArrayList<>();
-        for (Order item : list) {
-            if (item.getCurrency() == Currency.EUR) eurList.add(item);
-            else usdList.add(item);
+        for (Order order : list) {
+            if (order.getCurrency() == Currency.EUR) eurList.add(order);
+            else usdList.add(order);
         }
+//        eurList = list.stream().filter(order -> order.getCurrency() == Currency.EUR).collect(Collectors.toList());
+//        usdList = list.stream().filter(order -> order.getCurrency() == Currency.USD).collect(Collectors.toList());
         System.out.println("List, where currency is EUR" + eurList);
         System.out.println("List, where currency is USD" + usdList);
 
 //        separate list for as many lists as many unique cities are in User
         List<List<Order>> uniqueCities = new ArrayList<>();
-        Set<String> cities = new TreeSet<>();
+        Set<String> cities = new HashSet<>();
         List<Order> temp;
         for (Iterator<Order> it = list.listIterator(); it.hasNext();) {
             cities.add(it.next().getUser().getCity());
         }
-        for (String item: cities) {
+        for (String city: cities) {
             temp = new ArrayList<>();
             uniqueCities.add(temp);
             for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).getUser().getCity() == item) temp.add(list.get(i));
+                if (list.get(i).getUser().getCity() == city) temp.add(list.get(i));
             }
         }
         System.out.println("List by unique cities " + uniqueCities);
